b54281467754fb79ac4c5cd74af324e44f2b83a2
Rafael Oliveira <rafaoldev@gmail.com>
2023-05-05 15:59:21 -0300

    Updates

---
+++ precache-manifest.606290a21ebeaf83aa193d34e7f2f3b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49f22de4eeeb9ed8768d",
+    "url": "/digital-business-card/static/css/main.6af0cc04.chunk.css"
+  },
+  {
+    "revision": "49f22de4eeeb9ed8768d",
+    "url": "/digital-business-card/static/js/main.49f22de4.chunk.js"
+  },
+  {
+    "revision": "2c654338267428580a8f",
+    "url": "/digital-business-card/static/js/1.2c654338.chunk.js"
+  },
+  {
+    "revision": "2ac6fa471db2f3f58c76",
+    "url": "/digital-business-card/static/js/runtime~main.2ac6fa47.js"
+  },
+  {
+    "revision": "c6495dd817e7b70ddd840308c43c60b7",
+    "url": "/digital-business-card/static/media/small-pic.c6495dd8.jpeg"
+  },
+  {
+    "revision": "8b5c9a9172689b43fe2c19c0ae37cb0c",
+    "url": "/digital-business-card/index.html"
+  }
+];
