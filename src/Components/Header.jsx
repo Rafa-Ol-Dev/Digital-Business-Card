96e40ca0dcbe41b1896a1c887fbb2335537943d4
Rafael Oliveira <rafaoldev@gmail.com>
2023-05-12 14:56:00 -0300

    Update Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Foto from "../images/small-pic.jpeg";
+import Foto from "../images/foto.png";
 
 export default function Header() {
   return (
     <div className="header">
-      <img src={Foto} alt="minha foto de perfil" className="foto-perfil"/>
+      <img src={Foto} alt="minha foto de apresentação" className="foto-perfil"/>
     </div>
   )
 }
